2612fdd93f2eaaec09beb97f19918941d5b39105
Gaurav Sarma <gaurav4sarma@gmail.com>
2021-06-29 04:06:00 +0530

    Added leading zero function to make the timer look better

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -13,27 +13,36 @@ class Clock extends Component {
         }
     }
 
+    componentDidMount() {
+        this.getTimeUntil(this.props.deadline);
+        setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
+    }
+
+    leadingZero(num) {
+        return num < 10 ? '0' + num : num;
+    }
+
     getTimeUntil(deadline) {
-        
+
         const time = Date.parse(deadline) - Date.parse(new Date());
 
         const seconds = Math.floor((time / 1000) % 60);
-        const minutes = Math.floor(((time / 1000) % 60) % 60);
+        const minutes = Math.floor((time / 1000 / 60) % 60);
         const hours = Math.floor(time / (1000 * 60 * 60) % 24);
         const days = Math.floor(time / (1000 * 60 * 60 * 24));
 
-        console.log("Seconds ", seconds, " minutes ", minutes, " hours ", hours, " days", days);
+        this.setState({
+            days, hours, minutes, seconds
+        })
     }
 
     render() {
-        this.getTimeUntil(this.props.deadline);
-
         return (
             <div>
-                <div className="Clock-days">{this.state.days} days</div>
-                <div className="Clock-hours">{this.state.hours} hours</div>
-                <div className="Clock-minutes">{this.state.minutes} minutes</div>
-                <div className="Clock-seconds">{this.state.seconds} seconds</div>
+                <div className="Clock-days">{this.leadingZero(this.state.days)} days</div>
+                <div className="Clock-hours">{this.leadingZero(this.state.hours)} hours</div>
+                <div className="Clock-minutes">{this.leadingZero(this.state.minutes)} minutes</div>
+                <div className="Clock-seconds">{this.leadingZero(this.state.seconds)} seconds</div>
             </div>
         )
     }
